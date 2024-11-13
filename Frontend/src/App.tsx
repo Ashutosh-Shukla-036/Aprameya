103d55a3b4272aa49bc4819b89f69ba260af764c
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-13 23:46:37 +0530

    Added empty frontend folder for Aprameya project

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-3xl font-bold text-white">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
