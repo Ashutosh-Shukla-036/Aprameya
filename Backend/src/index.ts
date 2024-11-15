17e7d185339db7c8e8f80af594506622d5e7872d
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-15 12:15:49 +0530

    Updated Index file for backed with movie route

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from 'express';
+import fileRoute from "./routes/filmRoute"
+import { ConnectDB } from './db';
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
+
+ConnectDB();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Aprameya Production API!');
 });
 
+app.use('/films',fileRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
