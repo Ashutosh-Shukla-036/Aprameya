abd2e2056c847ef7567eeee2907dbbe0de2b76c1
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-20 23:44:48 +0530

    Added atom for films

---
+++ Frontend/src/Atoms/FilmAtom.tsx
@@ -0,0 +1,42 @@
+import { atom } from "recoil";
+
+interface Film {
+    title: string;
+    description: string;
+    genre: string;
+    director: string;
+    releaseDate: string;
+    link: string;
+    cast: string[];
+    crew?: {
+        dop?: string;
+        editing?: string;
+        dialogues?: string;
+        story?: string;
+        assistantDirectors?: string[];
+        coDirector?: string;
+        executiveProducer?: string;
+        coProducer?: string;
+        producer?: string;
+        music?: string;
+    };
+}
+
+export const FilmAtom = atom<Film[]>({
+    key: "FileAtom",
+    default: [],
+    effects: [
+        ({ setSelf, onSet }) => {
+            // Initialize from localStorage
+            const storedData = localStorage.getItem("FilmAtom");
+            if (storedData) {
+                setSelf(JSON.parse(storedData));
+            }
+
+            // Save to localStorage on changes
+            onSet((newValue) => {
+                localStorage.setItem("FilmAtom", JSON.stringify(newValue));
+            });
+        },
+    ],
+})
