1e0a045d9fe904e832ef40e39f737cab1004cc59
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-23 20:00:34 +0530

    Added user routes in index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
-import fileRoute from "./routes/filmRoute"
+import filmRoute from "./routes/filmRoute";
+import userRoute from "./routes/userRoute";
 import { ConnectDB } from './db';
 import cors from 'cors';
 
@@ -14,7 +15,8 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Aprameya Production API!');
 });
 
-app.use('/films',fileRoute);
+app.use('/api/user',userRoute);
+app.use('/api/films',filmRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
