ebd51f7ef5259846c89e07aab3fc9e0275e833aa
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-24 09:18:18 +0530

    Added recoilroot and react-router to main file

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';  
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </StrictMode>,
-)
+);
