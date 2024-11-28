08b25feaad09f23f131432a90b45cc027f2e5021
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-28 21:19:24 +0530

    Updated tailwind

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -1,11 +1,18 @@
 // tailwind.config.js
 export default {
+  lightMode : 'class',
+
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(90deg, rgba(33,33,33,1) 0%, rgba(69,69,69,1) 100%)',
+        'charcoal-metallic-dark': 'linear-gradient(90deg, rgba(33,33,33,1) 0%, rgba(55,55,55,1) 50%, rgba(69,69,69,1) 100%)',
+      },
+    },
   },
   plugins: [],
 }
