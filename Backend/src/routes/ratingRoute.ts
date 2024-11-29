1c9d7d1c36d2df54cc0f07365cb8dbe135b35fbe
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-29 23:29:29 +0530

    Updated rating route

--- Backend/src/routes/ratingRoute.ts
+++ Backend/src/routes/ratingRoute.ts
@@ -1,5 +1,5 @@
 import express  from "express";
-import { addOrUpdateRating, getRating } from "../controllers/ratingreviewControlller";
+import { addOrUpdateRating, getRating } from "../controllers/ratingController";
 const route = express.Router();
 
 route.post("/addrating", addOrUpdateRating);
